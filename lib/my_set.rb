a6dc229feb9e85e49b1d95e55f6d0f328ea58748
David <Davidmaxsands@gmail.com>
2022-03-14 17:27:44 -0600

    commit tree 1d402e4eb6edefc3603fa468393b39da40ea2647

--- lib/my_set.rb
+++ lib/my_set.rb
@@ -1,1 +1,36 @@
-# your code here
+def first_repeated_value(array)
+    set = Set.new
+
+    for i in 0..array.length
+        return array[i] if set.include?(array[i])
+        set.add(array[i])
+    end
+    nil
+end
+
+class MySet
+    def initialize(enumerable = [])
+        @hash = {}
+        enumerable.each do |value|
+            @hash[value] = true
+        end
+    end
+    
+    def include?(value)
+        @hash.has_key?(value)
+    end
+
+    def add(value)
+        @hash[value] = true
+        self
+    end
+
+    def delete(value)
+        @hash.delete(value)
+        self
+    end
+
+    def size 
+        @hash.size
+    end
+end
